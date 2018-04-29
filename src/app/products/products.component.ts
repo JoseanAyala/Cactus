9ded0374a5b3903e93b29ba987a46e182d4d544a
JoseanAyala <josean.ayala@upr.edu>
2018-04-28 22:56:25 -0400

    LocalStorage cart for items
    En products hice una funcion para que se pueda salvar el producto en un array de carro. Esto coge el existente cart y lo copia a typescript y a eso le añade el nuevo articulo y lo devuelve a localstorage

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+    AddingItemToCart(id, qty) {
+        const items: { productId: any , quantity }[] = [  JSON.parse(localStorage.getItem('cart')) ];
+        const newItems = { productId: id, quantity: qty };
+        items.push(newItems);
+        localStorage.removeItem('cart');
+        localStorage.setItem('cart', JSON.stringify(items));
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    constructor() { }
+    ngOnInit() { }
 
 }
